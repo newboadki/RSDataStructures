cd993792f1b2edf0d986a300130a5210ae924017
Borja Arias Drake <borja@Borjas-MacBook-Air.local>
2017-04-16 21:58:24 +0200

    Added unit tests for Rabin-Karp algorithm.

--- UnitTests/String-matching/RabinKarpAlgorithmTests.swift
+++ UnitTests/String-matching/RabinKarpAlgorithmTests.swift
@@ -10,9 +10,29 @@ import XCTest
 
 class RabinKarpAlgorithmTests: XCTestCase {
     
-    func testExample() {
-
-        let result = findOccurrences(of: "bcd", in: "abcdxccdbcdfghi", base: 256, primeNumber: 997)
-        XCTAssert(result  == [1, 8], "Found \(result)")
+    func testPatternFoundAtBeginning() {
+        let result = findOccurrences(of: "bcd", in: "bcdxccdbccfghi", base: 256, primeNumber: 997)
+        XCTAssert(result  == [0], "Found \(result)")
     }
+    
+    func testPatternFoundAtEnd() {
+        let result = findOccurrences(of: "bcd", in: "bcfghibcd", base: 256, primeNumber: 997)
+        XCTAssert(result  == [6], "Found \(result)")
+    }
+    
+    func testPatternIsTheText() {
+        let result = findOccurrences(of: "bcd", in: "bcdbcd", base: 256, primeNumber: 997)
+        XCTAssert(result  == [0,3], "Found \(result)")
+    }
+    
+    func testPatternFoundAtSeveralPlaces() {
+        let result = findOccurrences(of: "ddda", in: "abdddacdxccdbcdddafghi", base: 256, primeNumber: 997)
+        XCTAssert(result  == [2, 14], "Found \(result)")
+    }
+
+    func testPatternInLongerText() {
+        let result = findOccurrences(of: "bidsankadoutiwyq", in: "abcdxccdoiadshfakndkcnlkdmsafhaiuwekopoqwekmnbbvavcdsiahqoweioqimkjdszhgvabidsankadoutiwyqieofpmklsdzciouqiueuwyrquwehfijbcdfghi", base: 256, primeNumber: 997)
+        XCTAssert(result  == [74], "Found \(result)")
+    }
+
 }
