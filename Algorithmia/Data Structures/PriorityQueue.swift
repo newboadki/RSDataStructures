19e70aeca7f46762327c3296c0c14ceaf5849f75
Borja Arias Drake <borja.arias@gmail.com>
2017-06-10 20:02:55 +0100

    - Add comments to PriorityQueue protocol.

--- Algorithmia/Data Structures/PriorityQueue.swift
+++ Algorithmia/Data Structures/PriorityQueue.swift
@@ -19,11 +19,20 @@ public enum PriorityQueueError : Error {
 }
 
 
-
+/// Priority queues are queues where determining the min, max and extracting them are efficient operations.
 /// They mainly add semantics to a queue. Elements are returned in order according to some rule.
+///
+/// - min is expected to be O(1)
+/// - max is expected to be O(1)
+/// - extract min is expected to be O(log(N))
+/// - extract max is expected to be O(log(N))
 protocol PriorityQueue : Queue {
     
+    /// Priority queues can return results in ascendent or descendent order
     var type : PriorityQueueType { get }
-    
+        
+    /// Initializer
+    ///
+    /// - Parameter type: Type of the priority queue
     init(type: PriorityQueueType)
 }
